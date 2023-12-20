959dc62f567a06fcfd967690fecfad63f9b7fe24
robbinl <robbinl@telusplanet.net>
2023-12-20 11:33:39 -0700

    sequence

--- CPSC1012/e-sequence/Program.cs
+++ CPSC1012/e-sequence/Program.cs
@@ -1,14 +1,14 @@
 ﻿Demo1();
-Demo2();
-Demo3();
-Demo4();
-Demo5();
-Demo6();
-Demo7();
-Demo8();
-Demo9();
-Demo10();
-Demo11();
+//Demo2();
+//Demo3();
+//Demo4();
+//Demo5();
+//Demo6();
+//Demo7();
+//Demo8();
+//Demo9();
+//Demo10();
+//Demo11();
 
 void Demo1()
 {
