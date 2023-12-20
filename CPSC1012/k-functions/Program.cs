0cf93afecaa0063effa9bbfd6e3ff245a9d0a811
robbinl <robbinl@telusplanet.net>
2023-12-20 12:03:32 -0700

    structure of demos

---
+++ CPSC1012/k-functions/Program.cs
@@ -0,0 +1,67 @@
+﻿Demo1();
+//Demo2();
+//Demo3();
+//Demo4();
+//Demo5();
+//Demo6();
+//Demo7();
+//Demo8();
+//Demo9();
+//Demo10();
+//Demo11();
+
+void Demo1()
+{
+  
+}
+
+void Demo2()
+{
+  
+}
+
+void Demo3()
+{
+  
+}
+
+void Demo4()
+{
+  
+}
+
+void Demo5()
+{
+  
+}
+
+void Demo6()
+{
+  
+}
+
+void Demo7()
+{
+  
+}
+
+void Demo8()
+{
+  
+}
+
+void Demo9()
+{
+  
+}
+
+void Demo10()
+{
+  
+}
+
+void Demo11()
+{
+  
+}
+
